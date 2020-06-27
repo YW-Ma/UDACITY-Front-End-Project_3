ff14bd1ca2fad6c2c8c4ee6e40995b23c1e36cea
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-06-27 22:29:51 +0800

    update

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+const { json } = require('body-parser');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.post('/newData', (request, response) => {
+    const data = request.body;
+    if (data) {
+        projectData.city = data.name;
+        projectData.temperature = data.temperature;
+        projectData.date = data.date;
+        projectData.feelings = data.feelings;
+    }
+    response.send({message: 'Posted!'});
+});
 
+app.get('/getData', (request, response) => {
+    response.send(JSON.stringify(projectData));
+})
+
+const port = 8000;
 // Setup Server
+function listening() { console.log(`Server running on port:${port}`); }
+const server = app.listen(port, () => listening());
--- website/app.js
+++ website/app.js
@@ -1,5 +1,81 @@
 /* Global Variables */
+// API related variables
+const apiKey = 'c9267f5315ddb6b01cc3ac0e48b77f7a';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+// Elements in DOM
+const generateButton = document.getElementById('generate');
+const zipText = document.getElementById("zip");
+const feelingsText = document.getElementById('feelings');
+
+const date = document.querySelector('#date');
+const temp = document.querySelector('#temp');
+const content = document.querySelector('#content');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Obtain weather information from API
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
+const weatherInfo = async zip => await fetch(`${baseUrl+zip},us&appid=${apiKey}`);
+
+//POST --> to server
+const postData = async(url, weatherInfo) => {
+    const response = await fetch(url, {
+        method: 'Post',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(weatherInfo)
+    });
+    try {
+        const newData = await response.json();
+        return newData;
+
+    } catch (error) {
+        console.log('error:', error);
+    }
+}
+//GET --> from server
+function updateUI(data) {
+    date.innerHTML = "Date: " + data.date;
+    temp.innerHTML = "Temp: " + data.temperature;
+    content.innerHTML = "Feelings: " + data.feelings;
+}
+const getData = async(url) => {
+    const response = await fetch('getData');
+
+    try {
+        const data = await response.json();
+        updateUI(data);
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+// Generating
+generateButton.addEventListener('click', async() => {
+    
+    // 1. Change to Pending
+    generateButton.innerHTML = 'Pending . . .';
+    // 2. Obtain values from user
+    const zip = zipText.value;
+    const feelings = feelingsText.value;
+    
+    // 3. Obtain data from weatherInfo
+    const response = await weatherInfo(zip);
+    const res = await response.json();
+    
+    // 4. Parsing data from weatherInfo
+    const temp = res.main.temp.toString() + '°C';
+    const city = res.name.toString();
+    const dataset = { name: city, temperature: temp, date: newDate, feelings: feelings };
+    
+    // 5. Posting data to Server
+    data = await postData('/newData', dataset);
+    
+    // 6. Updating UI based on data in Server
+    getData('getAll');
+
+    // 7. Back to Generate
+    generateButton.innerHTML = 'Generate';
+});
