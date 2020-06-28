568b3f65c3d44718918f2d46eb62db80703a5232
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-06-28 11:06:56 +0800

    adjust the month.

--- website/app.js
+++ website/app.js
@@ -13,7 +13,7 @@ const content = document.querySelector('#content');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
 // Obtain weather information from API
 // api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
